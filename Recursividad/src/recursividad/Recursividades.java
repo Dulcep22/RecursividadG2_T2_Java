4ad5da5baf0fef3508c84ce8520eee538f5e5b92
Dulcep22 <25030393@itesa.edu,mx>
2026-09-13 20:19:45 -0600

    Version_01: Proyecto vacío

--- Recursividad/src/recursividad/Recursividades.java
+++ Recursividad/src/recursividad/Recursividades.java
@@ -11,56 +11,5 @@ package recursividad;
 
 public class Recursividades {
 
-    // FACTORIAL
-    public int calcularFactorial(int num) {
-        // Caso base
-        if (num == 1) {
-            return 1;
-        }
-
-        // Caso general
-        return num * calcularFactorial(num - 1);
-    }
-
-    // EJERCICIO 1
-    public int contarVocales(String texto) {
-        // Caso base
-        if (texto.length() == 0) {
-            return 0;
-        }
-
-        char letra = Character.toLowerCase(texto.charAt(0));
-
-        int vocal = 0;
-
-        if (letra == 'a' || letra == 'e' || letra == 'i'
-                || letra == 'o' || letra == 'u') {
-            vocal = 1;
-        }
-
-        // Llama nuevamente al método con el resto de la cadena
-        return vocal + contarVocales(texto.substring(1));
-    }
-
-    // EJERCICIO 2
-    public int sumarDigitos(int num) {
-        // Caso base
-        if (num < 10) {
-            return num;
-        }
-
-        // Caso general
-        return (num % 10) + sumarDigitos(num / 10);
-    }
-
-    // EJERCICIO 3
-    public int sumarPrimerosNumeros(int n) {
-        // Caso base
-        if (n == 1) {
-            return 1;
-        }
-
-        // Caso general
-        return sumarPrimerosNumeros(n - 1) + n;
-    }
+    
 }
